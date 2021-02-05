b9582e9ad81f9e8a87b820d1f06f9cc365149cd5
Antoine Chassagne <antoine.chassagne@me.com>
2021-02-05 11:39:58 +0100

    Add Mailer global service

---
+++ server/services/Mailer.js
@@ -0,0 +1,20 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.APP_EMAIL_ADDRESS,
+    pass: process.env.APP_EMAIL_PASSWORD,
+  },
+});
+
+const sendMail = (options) => {
+  return new Promise((resolve, reject) => {
+    transporter.sendMail({ ...options, from: process.env.APP_EMAIL_ADDRESS }, (error) => {
+      if (error) return reject(error);
+      return resolve();
+    });
+  });
+};
+
+module.exports = { sendMail };
