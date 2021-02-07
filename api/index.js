6636ab80e55ae1f14bd50dfeaa03b3cc512ba52e
Antoine Chassagne <antoine.chassagne@me.com>
2021-02-07 10:34:51 +0100

    Remove api's fallback port

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Listening on port ${process.env.PORT || 3000}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
 });
